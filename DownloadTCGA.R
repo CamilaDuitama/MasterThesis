63ad0ada4a954eac24ed64e45892da9bb6f2118b
CamilaDuitama <33686652+CamilaDuitama@users.noreply.github.com>
2019-03-19 23:35:36 -0500

    Initial data exploration progress

--- DownloadTCGA.R
+++ DownloadTCGA.R
@@ -20,8 +20,22 @@ GBM_FPKM_TCGA<-assays(expdat)
 ####Using RTCGA Toolbox
 stddata <- getFirehoseRunningDates()
 stddata
+# clinical,miRNASeqGene(0*0), RNASeq2GeneNorm(0*0),Methylation,
+#mRNAArray,miRNAArray,GISTIC,RNAseqNorm,RNAseq2Norm
 GBMData <- getFirehoseData (dataset="GBM", runDate="20160128",
-                            clinic=TRUE,, mRNAArray=TRUE,miRNAArray=TRUE,Methylation=TRUE,
-                            CNASNP=TRUE,CNASeq=TRUE, RNAseqNorm=TRUE)
-GBM_RTCGA_RNASeq<-getData(GBMData,"RNASeq2GeneNorm")
-GBM_RTCGA_mRNAArray<-GBMData@mRNAArray[[3]]@DataMatrix
+                            clinic=TRUE,mRNASeq= TRUE, mRNAArray=TRUE,miRNAArray=TRUE,Methylation=TRUE,
+                            GISTIC =TRUE, RNAseqNorm= TRUE, RNAseq2Norm=TRUE,miRNASeqGene=TRUE)
+clinicData <- getData(GBMData,"clinical")
+mRNAArray <- getData(GBMData,"mRNAArray",platform = )
+write.table(GBMData@clinical, file = "Clinical.csv")
+write.table(GBMData@miRNASeqGene,file="miRNASeqGene.csv")
+write.table(GBMData@RNASeqGene,file="RNASeqGene.csv")
+write.table(GBMData@RNASeq2GeneNorm,file="RNASeq2GeneNorm.csv")
+write.csv(GBMData@mRNAArray[[3]]@DataMatrix,file="gdac.broadinstitute.org_GBM.Merge_transcriptome__ht_hg_u133a__broad_mit_edu__Level_3__gene_rma__data.Level_3.2016012800.0.0.csv")
+write.csv(GBMData@mRNAArray[[1]]@DataMatrix,file="gdac.broadinstitute.org_GBM.Merge_mirna__h_mirna_8x15k__unc_edu__Level_3__unc_DWD_Batch_adjusted__data.Level_3.2016012800.0.0.csv")
+write.csv(GBMData@Methylation[[1]]@DataMatrix,file="gdac.broadinstitute.org_GBM.Merge_methylation__humanmethylation27__jhu_usc_edu__Level_3__within_bioassay_data_set_function__data.Level_3.2016012800.0.0.csv")
+write.csv(GBMData@GISTIC@AllByGene,file="GISTIC_AllByGene.csv")
+
+diffGeneExprs <- getDiffExpressedGenes(dataObject=GBMData,DrawPlots=TRUE,
+                                       adj.method="BH", adj.pval=0.05, raw.pval=0.05, logFC=2, hmTopUpN=100,
+                                       hmTopDownN=100)
